f99c1273f55f2d54e06e867fb7c8c70ccf2e9288
ssandif <ssandifer@umbc.edu>
2020-05-03 20:41:42 -0400

    Add alpha-beta pruning and enchance command line

--- ParallelRisk/AlphaBeta.cs
+++ ParallelRisk/AlphaBeta.cs
@@ -2,9 +2,14 @@
 
 namespace ParallelRisk
 {
-    public static class Minimax
+    public static class AlphaBeta
     {
         public static TMove Serial<TState, TMove>(TState node, int depth)
+            where TState : IState<TMove>
+            where TMove : IMove<TState>
+        => Serial<TState, TMove>(node, depth, double.NegativeInfinity, double.PositiveInfinity);
+
+        private static TMove Serial<TState, TMove>(TState node, int depth, double alpha, double beta)
             where TState : IState<TMove>
             where TMove : IMove<TState>
         {
@@ -16,9 +21,13 @@ namespace ParallelRisk
                 (TMove Move, double Utility) value = (default, double.NegativeInfinity);
                 foreach (TMove move in node.Moves())
                 {
-                    double newUtil = SerialEstimatedOutcome<TState, TMove>(move, depth);
+                    double newUtil = SerialEstimatedOutcome<TState, TMove>(move, depth, alpha, beta);
                     if (newUtil > value.Utility)
                         value = (move, newUtil);
+
+                    alpha = Max(alpha, value.Utility);
+                    if (alpha >= beta)
+                        break;
                 }
                 return value.Move;
             }
@@ -27,15 +36,19 @@ namespace ParallelRisk
                 (TMove Move, double Utility) value = (default, double.PositiveInfinity);
                 foreach (TMove move in node.Moves())
                 {
-                    double newUtil = SerialEstimatedOutcome<TState, TMove>(move, depth);
+                    double newUtil = SerialEstimatedOutcome<TState, TMove>(move, depth, alpha, beta);
                     if (newUtil < value.Utility)
                         value = (move, newUtil);
+
+                    beta = Min(beta, value.Utility);
+                    if (alpha >= beta)
+                        break;
                 }
                 return value.Move;
             }
         }
 
-        private static double SerialMinimax<TState, TMove>(TState node, int depth)
+        private static double SerialAlphaBeta<TState, TMove>(TState node, int depth, double alpha, double beta)
             where TState : IState<TMove>
             where TMove : IMove<TState>
         {
@@ -46,26 +59,38 @@ namespace ParallelRisk
             {
                 double value = double.NegativeInfinity;
                 foreach (TMove atk in node.Moves())
-                    value = Max(value, SerialEstimatedOutcome<TState, TMove>(atk, depth));
+                {
+                    value = Max(value, SerialEstimatedOutcome<TState, TMove>(atk, depth, alpha, beta));
+
+                    alpha = Max(alpha, value);
+                    if (alpha >= beta)
+                        break;
+                }
                 return value;
             }
             else
             {
                 double value = double.PositiveInfinity;
                 foreach (TMove move in node.Moves())
-                    value = Min(value, SerialEstimatedOutcome<TState, TMove>(move, depth));
+                {
+                    value = Min(value, SerialEstimatedOutcome<TState, TMove>(move, depth, alpha, beta));
+
+                    beta = Min(beta, value);
+                    if (alpha >= beta)
+                        break;
+                }
                 return value;
             }
         }
 
-        private static double SerialEstimatedOutcome<TState, TMove>(TMove node, int depth)
+        private static double SerialEstimatedOutcome<TState, TMove>(TMove node, int depth, double alpha, double beta)
             where TState : IState<TMove>
             where TMove : IMove<TState>
         {
             double value = 0;
             foreach ((double probability, TState outcome) in node.Outcomes())
             {
-                value += probability * SerialMinimax<TState, TMove>(outcome, depth - 1);
+                value += probability * SerialAlphaBeta<TState, TMove>(outcome, depth - 1, alpha, beta);
             }
             return value;
         }
--- ParallelRiskConsole/Program.cs
+++ ParallelRiskConsole/Program.cs
@@ -1,43 +1,63 @@
 ﻿using System;
+using System.Diagnostics;
+using CommandLine;
 using ParallelRisk;
 
 namespace ParallelRiskConsole
 {
     class Program
     {
+        class Options
+        {
+            [Option('d', "maxdepth", Required = false, Default = 10, HelpText = "Maximum depth to search to.")]
+            public int MaxDepth { get; set; }
+
+            [Option('a', "alphabeta", Required = false, Default = false, HelpText = "Whether or not to use alpha-beta pruning.")]
+            public bool AlphaBeta { get; set; }
+
+            [Option('p', "parallel", Required = false, Default = false, HelpText = "Use the MPI-enabled version of the algorithm.")]
+            public bool Parallel { get; set; }
+
+            [Option('t', "time", Required = false, Default = false, HelpText = "Times the program runtime.")]
+            public bool Time { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            int MaxDepth = 10;
-            if (args.Length > 0)
-            {
-                int potentialDepth = int.Parse(args[0]);
+            Parser.Default.ParseArguments<Options>(args).WithParsed(MainWithOptions);
+        }
 
-                if (potentialDepth > 0)
-                    MaxDepth = potentialDepth;
-            }
+        static void MainWithOptions(Options options)
+        {
+            Stopwatch stopwatch = null;
 
             BoardState board = Risk.StandardBoard();
-            Move move = Minimax.Serial<BoardState, Move>(board, MaxDepth);
-            
 
+            if (options.Time)
+            {
+                stopwatch = new Stopwatch();
+                stopwatch.Start();
+            }
+
+            Move move = (options.AlphaBeta, options.Parallel) switch
+            {
+                (true, true) => throw new NotImplementedException(),
+                (true, false) => AlphaBeta.Serial<BoardState, Move>(board, options.MaxDepth),
+                (false, true) => throw new NotImplementedException(),
+                (false, false) => Minimax.Serial<BoardState, Move>(board, options.MaxDepth)
+            };
+
+            stopwatch?.Stop();
 
             if (move.IsAttack)
                 Console.WriteLine($"Attack from {(Risk.Id)move.FromId} to {(Risk.Id)move.ToId}");
+            else if (move.FortifyCount > 0)
+                Console.WriteLine($"Pass turn and fortify {move.FortifyCount} troops from {(Risk.Id)move.FromId} to {(Risk.Id)move.ToId}");
             else
-                Console.WriteLine($"PassTurn");
+                Console.WriteLine("Pass turn and don't fortify.");
 
-            /*
-            Move reinf = Minimax.SerialMove<BoardState, Move>(board, MaxDepth);
-
-            Console.WriteLine($"{(Move.MoveType)reinf.Action}");
-            if (reinf.Action == Move.MoveType.Reinforce) {
-                if (reinf.From.Id != reinf.To.Id) {
-                    Console.WriteLine($"Reinforce from {(Risk.Id)reinf.FromId} to {(Risk.Id)reinf.ToId}");
-                } else {
-                    Console.WriteLine($"Recommended no change.");
-                }
-            } */
-            
+            if (options.Time)
+                Console.WriteLine($"Runtime (s): {stopwatch.Elapsed.TotalSeconds}");
         }
     }
 }
